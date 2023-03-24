08368d6b5bfd1c41a8c7efc334757045d31ce3b6
Ozan <akcebe.ozn@gmail.com>
2023-03-24 10:02:19 +0100

    basic Websocket structure

--- FlappyBallsServer/FlappyBallsServer/Program.cs
+++ FlappyBallsServer/FlappyBallsServer/Program.cs
@@ -1,6 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using SocketLibrary;
 
-app.Run();
+SocketAction socketAction = new SocketAction();
+
+var builder = new WebHostBuilder()
+    .UseKestrel()
+    .Configure(
+        app =>
+        {
+            var webSocketOptions = new WebSocketOptions
+            {
+                KeepAliveInterval = TimeSpan.FromMinutes(2)
+            };
+
+            app.UseWebSockets(webSocketOptions);
+
+            app.Run(async (context) =>
+            {
+                if (context.Request.Path == "/")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        socketAction.Connect(context.WebSockets);
+                        await Task.Run(() => {
+                            while (true)
+                            {
+                            }
+                        });
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    }
+                }
+            });
+        })
+    .Build();
+builder.Run();
